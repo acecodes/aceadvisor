32af85583212f4a1aba66660b3d27800a1400929
Ace <ace.github@gmail.com>
2015-05-29 14:42:48 -0700

    More performance tweaks

--- static/js/aceadvisor.js
+++ static/js/aceadvisor.js
@@ -1,43 +1,40 @@
-// Change script for markets box
-$(document).ready(function () {
-    $('.bloomberg').hide();
-    $('#bloomberg1').show();
-    $('#selectField-bloomberg').change(function () {
+var AceAdvisor = (function() {
+    $(document).ready(function() {
         $('.bloomberg').hide();
-        $('#'+$(this).val()).show();
+        $('#bloomberg1').show();
+        $('#selectField-bloomberg').change(function() {
+            $('.bloomberg').hide();
+            $('#' + $(this).val()).show();
+        });
     });
-});
 
-$(document).ready(function () {
-    $('.stocks').hide();
-    $('#stocks1').show();
-    $('#selectField-stocks').change(function () {
+    $(document).ready(function() {
         $('.stocks').hide();
-        $('#'+$(this).val()).show();
+        $('#stocks1').show();
+        $('#selectField-stocks').change(function() {
+            $('.stocks').hide();
+            $('#' + $(this).val()).show();
+        });
     });
-});
 
-$( "#headlines" ).click(function() {
-  $( "#top" ).toggle( "slow", function() {
-  });
-});
+    $('#headlines').click(function() {
+        $('#top').toggle('slow', function() {});
+    });
 
-$( "#markets" ).click(function() {
-  $( "#middle" ).toggle( "slow", function() {
-  });
-});
+    $('#markets').click(function() {
+        $('#middle').toggle('slow', function() {});
+    });
 
-$( "#screener" ).click(function() {
-  $( "#bottom" ).toggle( "slow", function() {
-  });
-});
+    $('#screener').click(function() {
+        $('#bottom').toggle('slow', function() {});
+    });
 
-// $( "#options" ).click(function() {
-//   $( "#options_area" ).toggle( "slow", function() {
-//   });
-// });
+    // $( '#options' ).click(function() {
+    //   $( '#options_area' ).toggle( 'slow', function() {
+    //   });
+    // });
 
-$( ".alert-box" ).click(function() {
-  $( ".alert-box" ).toggle( "slow", function() {
-  });
-});
+    $('.alert-box').click(function() {
+        $('.alert-box').toggle('slow', function() {});
+    });
+})();
